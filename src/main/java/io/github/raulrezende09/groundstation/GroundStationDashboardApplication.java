5d0c3365fde9a819ac471321d02dbce6401fea6e
RaulRezende09 <raurede21@gmail.com>
2026-09-13 01:01:12 -0300

    chore: initialize Spring Boot project structure

---
+++ src/main/java/io/github/raulrezende09/groundstation/GroundStationDashboardApplication.java
@@ -0,0 +1,13 @@
+package io.github.raulrezende09.groundstation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroundStationDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroundStationDashboardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/raulrezende09/groundstation/GroundStationDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.raulrezende09.groundstation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroundStationDashboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
